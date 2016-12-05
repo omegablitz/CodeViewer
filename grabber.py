63f42917e307b2f0563f03bc311d59ced11e93f2
Aashish <aashishwelling@gmail.com>
2016-12-05 23:56:02 +0000

    Quick fix

--- grabber.py
+++ grabber.py
@@ -2,8 +2,16 @@ import sys
 lines = sys.stdin.readlines()
 packages = {}
 for line in lines:
+    package = None
+    file = None
     line = line.strip()[2:]
-    package, file = line.split('/', 1)
+    if '/' in line:
+        package, file = line.split('/', 1)
+    else:
+        file = line
+        package = line.split('.html')[0]
+    # print(package + ", " + line)
+    # print line
     line = '''<li><a id="''' + line + '''" class="file" href="javascript:void(0)">''' + file.split('.html')[0] + '''</a></li>'''
     try:
         packages[package].append(line)
@@ -11,7 +19,7 @@ for line in lines:
         packages[package] = [line]
 # print packages
 for package in packages:
-    print '''<li class="dropdown"><a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">''' + package + ''' <span class="caret"></span></a><ul class="dropdown-menu">'''
+    print('''<li class="dropdown"><a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">''' + package + ''' <span class="caret"></span></a><ul class="dropdown-menu">''')
     for l in packages[package]:
-        print l
-    print '''</ul></li>'''
+        print(l)
+    print('''</ul></li>''')
